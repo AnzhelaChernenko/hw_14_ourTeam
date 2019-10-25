cd136140c33f8943ad4307f406e32de3fb0bf56f
Chernenko Anzhela <53837621+AnzhelaChernenko@users.noreply.github.com>
2019-10-25 22:21:30 +0300

    add index

---
+++ main.d0d36a28073422acdc69.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/file-loader/dist/cjs.js?!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/scss/styles.scss":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5!./src/scss/styles.scss ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: unclosed parenthesis in media query expression\\n        on line 297 of G:\\\\Beetroot\\\\Домашние задания\\\\13.b\\\\src\\\\scss\\\\styles.scss\\n>> @media screen and (max width: 1200px){\\r\\n   -------------------^\\n\");\n\n//# sourceURL=webpack:///./src/scss/styles.scss?./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5");
+
+/***/ })
+
+})
